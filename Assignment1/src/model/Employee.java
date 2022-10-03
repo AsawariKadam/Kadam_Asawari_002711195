75a32b8d2b0d96672c9deff4b2d50afe44a2ebef
AsawariKadam <kadam.asa@northeastern.edu>
2022-10-02 22:36:34 -0400

    Adding MainJFrame,CreateJPanel and ViewJPanel

---
+++ Assignment1/src/model/Employee.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author asawari
+ */
+public class Employee{
+    private String name;
+    private String id;
+    private String age;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String teamInfo;
+    private String title;
+    private String contNum;
+    private String contEmail;
+    private  String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContNum() {
+        return contNum;
+    }
+
+    public void setContNum(String contNum) {
+        this.contNum = contNum;
+    }
+
+    public String getContEmail() {
+        return contEmail;
+    }
+
+    public void setContEmail(String contEmail) {
+        this.contEmail = contEmail;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString(){
+       return name;
+    }
+
+   
+
+}
